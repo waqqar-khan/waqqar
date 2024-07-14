5c28db6f8126d24b5c1b250739ee850edbb5acec
waqqar-khan <waqqarkhan456@gmail.com>
2024-07-14 20:24:56 +0530

    fix header and home responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import '../App.css';
+import { useState } from "react";
+import "../App.css";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,28 +12,41 @@ const Header = () => {
     <header>
       <div className="header-content">
         <h1>WAQQAR KHAN</h1>
-        <div className={`menu-icon ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div
+          className={`menu-icon ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
           <div className="bar"></div>
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
       </div>
-      <nav className={`nav-menu ${isMenuOpen ? 'open' : ''}`}>
+      <nav className={`nav-menu ${isMenuOpen ? "open" : ""}`}>
         <ul>
           <li>
-            <a href="#home" onClick={toggleMenu}>Home</a>
+            <a href="#home" onClick={toggleMenu}>
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about-me" onClick={toggleMenu}>About Me</a>
+            <a href="#about-me" onClick={toggleMenu}>
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#projects" onClick={toggleMenu}>Projects</a>
+            <a href="#projects" onClick={toggleMenu}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#recommendations" onClick={toggleMenu}>Recommendations</a>
+            <a href="#recommendations" onClick={toggleMenu}>
+              Recommendations
+            </a>
           </li>
           <li>
-            <a href="#contact-me" onClick={toggleMenu}>Contact Me</a>
+            <a href="#contact-me" onClick={toggleMenu}>
+              Contact Me
+            </a>
           </li>
         </ul>
       </nav>
